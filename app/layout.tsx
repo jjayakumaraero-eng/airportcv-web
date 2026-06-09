ad63afff7f120db5da5d7dbcdccc2396ef1692ef
Jayakumar Jayaraman <jjayakumar.aero@gmail.com>
2026-06-09 13:09:11 +0100

    Add Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,15 @@ export default function RootLayout({
             gtag('config', 'G-TY2MBRC7F6');
           `}
         </Script>
+        <Script id="microsoft-clarity">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "x4btwtfqfb");
+  `}
+</Script>
 
         {children}
       </body>
